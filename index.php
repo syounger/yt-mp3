ceabb58e9e58f3e68936d032b270f69d4e046f66
Simcha <simcha.younger@officeearth.com>
2017-11-12 00:10:05 +0200

    main api file

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once('bootstrap.php');
+
+use YoutubeScraper\YoutubeScraper;
+
+Flight::map('download', function($file, $filename='', $mime='application/octet-stream'){
+		if(!filename && !file_exists($file)) throw new DomainException('Filename must be provided to download raw data');
+		if(!$filename) $filename = basename($file);
+
+		header('Content-Disposition: Attachment;filename='.$filename);
+		header('Content-type: '.$mime);
+		if(file_exists($file)){
+			header('Content-length: '.filesize($file));
+			readfile($file);
+		}else{
+			header('Content-length: '.strlen($file));
+			echo $file;
+		}
+	});
+
+// url-to-mp3
+Flight::route("GET /api/youtube2mp3", function(){
+	try{
+		$yt = new yt_downloader(Flight::request()->query->url, true);
+		$video = new Video($yt->video);
+		$mp3 = $yt->createMP3();
+		Flight::json(["video_id" => $yt->id, "url" => '/api/mp3/'.$mp3]);
+	}catch(Exception $e){
+throw $e;
+		Flight::notFound();
+	}
+	});
+
+Flight::route('GET /api/mp3/{mp3}', function($mp3){
+		$dir = Video::$audio_dir;
+		$file = $dir.DIRECTORY_SEPARATOR.$mp3;
+		if(!file_exists($file)) Flight::notFound();
+		Flight::download($file);
+	});
+
+Flight::start();
