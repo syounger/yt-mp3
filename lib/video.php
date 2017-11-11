992a3e00b01682a506344874ce06e57e42e2af85
Simcha <simcha.younger@officeearth.com>
2017-11-12 00:02:01 +0200

    very basic video class, for converting to mp3

---
+++ lib/video.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('youtube-dl.config.php');
+
+class Video{
+
+
+	public $name;
+	public $dir = cnfg::Download_Folder;
+	public static $audio_dir = 'MP3';
+	private $path;
+
+	public function __construct($name){
+		$this->name = $name;
+		$this->path = $this->dir.DIRECTORY_SEPARATOR.$this->name;
+	}
+
+	/**
+	 * @brief create mp3 from video
+	 * @return string path to mp3 file
+	 * @throws Exception if mp3 not created
+	 */	
+	public function createMP3(){
+		$mp3 = str_replace('mp4', 'mp3', $this->name);
+		$mp3_path = $this->audio_dir.DIRECTORY_SEPARATOR.$mp3;
+		`ffmpeg -i {$this->path} {$mp3_path}`;
+		if(!file_exists($mp3_path)) throw new Exception('failed to generate mp3 file');
+		return $mp3_path;
+	}
+
+
+}
