a187a307f3910817ce36e3776b5d0c2bdced42b9
Simcha <simcha.younger@officeearth.com>
2017-11-12 01:49:24 +0200

    syntax and output format fixes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('bootstrap.php');
 use YoutubeScraper\YoutubeScraper;
 
 Flight::map('download', function($file, $filename='', $mime='application/octet-stream'){
-		if(!filename && !file_exists($file)) throw new DomainException('Filename must be provided to download raw data');
+		if(!$filename && !file_exists($file)) throw new DomainException('Filename must be provided to download raw data');
 		if(!$filename) $filename = basename($file);
 
 		header('Content-Disposition: Attachment;filename='.$filename);
@@ -26,14 +26,14 @@ Flight::route("GET /api/youtube2mp3", function(){
 		$yt = new yt_downloader(Flight::request()->query->url, true);
 		$video = new Video($yt->video);
 		$mp3 = $video->createMP3();
-		Flight::json(["video_id" => $yt->id, "url" => '/api/mp3/'.$mp3]);
+		Flight::json(["video_id" => $yt->videoID, "url" => $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/api/mp3/'.$mp3]);
 	}catch(Exception $e){
-throw $e;
+				//throw $e;
 		Flight::notFound();
 	}
 	});
 
-Flight::route('GET /api/mp3/{mp3}', function($mp3){
+Flight::route('GET /api/mp3/@mp3', function($mp3){
 		$dir = Video::$audio_dir;
 		$file = $dir.DIRECTORY_SEPARATOR.$mp3;
 		if(!file_exists($file)) Flight::notFound();
